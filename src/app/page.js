3943e824483c2afaab9467a42a3d274e4eb03abc
Ronja <roba0001@stud.kea.dk>
2024-11-15 13:06:46 +0100

    forside done

--- src/app/page.js
+++ src/app/page.js
@@ -8,17 +8,13 @@ export default function Home() {
       <Meny />
       <br></br>
       <main className="col-start-2">
-        <div className="col-span-full z-index 1">
+        <div className="max-h-svh col-span-full">
           <Image
             width={1500}
-            height={100}
+            height={1500}
             alt="Forside billede"
             src="/images/forside_img.jpg"
           ></Image>
-        </div>
-        <div className="col-span-full z-index 2">
-          <h1>Simple Store</h1>
-          <Link href={"pages/products"}>See products!</Link>
         </div>
       </main>
     </body>
