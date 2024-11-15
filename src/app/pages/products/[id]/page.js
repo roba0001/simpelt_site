b934e4160bfd57ef098a0acd039135ce622048fe
Astrid Marie Lindholm <asli0001@stud.kea.dk>
2024-11-15 12:49:28 +0100

    4

--- src/app/pages/products/[id]/page.js
+++ src/app/pages/products/[id]/page.js
@@ -17,7 +17,7 @@ export default async function ProductPage({ params }) {
         <p>Back</p>
       </Link>
 
-      <div className="grid grid-cols-2 place-items-center gap-8 bg-[rgb(255, 250, 239)]">
+      <div className="grid grid-cols-2 place-items-center gap-8 bg-card border border-accent rounded-2xl">
         <div className="flex flex-col items-center">
           <Image
             src={product.thumbnail}
@@ -27,7 +27,7 @@ export default async function ProductPage({ params }) {
             className="rounded-lg shadow-lg"
           />
 
-          <div className="flex justify-center gap-2 mt-4">
+          <div className="flex justify-center gap-2 mt-4 ">
             {product.images?.map((image, index) => (
               <Image
                 key={index}
@@ -35,29 +35,41 @@ export default async function ProductPage({ params }) {
                 width={75}
                 height={75}
                 alt="Product image"
-                className="rounded-md border border-bfg-[rgb(232, 226, 197)] shadow-sm"
+                className="rounded-md  shadow-sm"
               />
             ))}
           </div>
         </div>
 
         <div className="space-y-4">
-          <h1 className="text-2xl font-semibold ">{product.title}</h1>
-          <p className="text-lg text-gray-600">Category: {product.category}</p>
-          <p className="text-gray-500 leading-relaxed">{product.description}</p>
+          <h1 className="text-2xl font-semibold text-accent ">
+            {product.title}
+          </h1>
+          <p className="text-lg">Category: {product.category}</p>
+          <p>stock: {product.stock}</p>
+          <p className=" leading-relaxed mr-3">{product.description}</p>
+          <p> Price: {product.price}</p>
+          <button className="bg-accent rounded-2xl p-2 hover:bg-accenthover">
+            Add to basket
+          </button>
         </div>
       </div>
 
       <h1 className="mt-8 text-xl font-semibold ">Reviews:</h1>
-      <div className="flex  gap-5 justify-start mt-4 --card">
+      <div className="flex  gap-5 justify-start mt-4 ">
         {product.reviews?.map((review, index) => (
           <div
             key={index}
-            className="w-full md:w-1/3 bg-white p-4 rounded-lg shadow-md border border-gray-200"
+            className="w-full md:w-1/3 bg-card p-4  shadow-md border border-accent rounded-2xl"
           >
-            <h2 className=" flex text-lg font-semibold">
+            <h2 className="flex text-lg font-semibold">
               {[...Array(review.rating)].map((_, index) => (
-                <IoStarOutline key={index} />
+                <IoStarOutline
+                  key={index}
+                  className={`text-${
+                    review.rating >= 4 ? "yellow-500" : "gray-400"
+                  }`}
+                />
               ))}{" "}
               - {review.comment}
             </h2>
