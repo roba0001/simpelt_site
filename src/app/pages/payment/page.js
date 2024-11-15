d49cfceabf71e4a7804a58a2fb2793f1a8656471
Astrid Marie Lindholm <asli0001@stud.kea.dk>
2024-11-15 08:46:59 +0100

    betal nu

--- src/app/pages/payment/page.js
+++ src/app/pages/payment/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 export default function PaymentPage() {
   const searchParams = useSearchParams();
@@ -14,9 +15,23 @@ export default function PaymentPage() {
   };
 
   const parsedItems = items ? parseItems(items) : [];
+
   const parsedItem = parsedItems.map((parsedItem) => ({ parsedItem }));
 
   console.log(Array.from(parsedItem));
 
-  //   tag fat i array gennem url'en
+  return (
+    <div>
+      <h1>Betaling</h1>
+      <p>Du har valgt følgende produkter:</p>
+      <ul>
+        {parsedItems.map((item) => (
+          <li key={item.id}>Produkt ID: {item.id}</li>
+        ))}
+      </ul>
+      <button>
+        <Link href={`/pages/payment?items=${items}`}>Betal nu!</Link>
+      </button>
+    </div>
+  );
 }
