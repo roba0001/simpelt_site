757817896e8b79b53ca8372a01980a9c92bd9d3d
Astrid Marie Lindholm <asli0001@stud.kea.dk>
2024-11-15 10:47:24 +0100

    nn

--- src/app/components/Meny.js
+++ src/app/components/Meny.js
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function Meny() {
   return (
-    <nav>
-      <ul className={`flex justify-evenly p-2`}>
-        <Link href={"/"}>
-          <li>Home</li>
-        </Link>
-        <Link href={"/pages/products"}>
-          <li>Products</li>
-        </Link>
+    <nav className="bg-[rgb(242, 232, 206)]">
+      <ul className="flex justify-evenly p-2">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/pages/products">Products</Link>
+        </li>
       </ul>
     </nav>
   );
